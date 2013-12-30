0a4143968b01e700d941c609e6850abad42e1a17
James Kelly <jkelly2510@gmail.com>
2013-12-30 17:48:15 +0000

    Log in to Betfair without hardcoded password

--- bft/bft.py
+++ bft/bft.py
@@ -13,9 +13,14 @@ import simplejson as json
 import urllib2
 
 
+
+user = raw_input('Betfair Username:')
+passw = raw_input('Betfair Password:')
+
+
 bf = bfpy.BfClient()
 try:
-  bfresp  = bf.login(username='kelbet86', password='nooooo')
+  bfresp  = bf.login(username=user, password=passw)
   print "logged in"
 except bfpy.BfError, e:
   print "login error: %s" % str(e)
