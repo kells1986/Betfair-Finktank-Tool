842005740bfe64fec91834f0e22c53ceb7dd3e1b
James Kelly <jkelly2510@gmail.com>
2013-12-30 15:56:27 +0000

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Betfair-Finktank-Tool',
+      version='1.0',
+      description='Compare Finktank predictions with Betfair odds',
+      author='James Kelly',
+      author_email='jkelly2510@gmail.com',
+      url='https://github.com/kells1986/Betfair-Finktank-Tool'
+     )
