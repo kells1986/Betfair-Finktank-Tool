b7d8f92807983da7af67af36be93ed3fed7c8925
James Kelly <jkelly2510@gmail.com>
2013-12-30 17:58:37 +0000

    Tidied log in into function

--- bft/bft.py
+++ bft/bft.py
@@ -13,17 +13,19 @@ import simplejson as json
 import urllib2
 import getpass
 
+def log_in():
+	user = raw_input('Betfair Username:')
+	passw = getpass.getpass('Betfair Password:')
 
-user = raw_input('Betfair Username:')
-passw = getpass.getpass('Betfair Password:')
+	bf = bfpy.BfClient()
+	try:
+		bfresp  = bf.login(username=user, password=passw)
+	except bfpy.BfError, e:
+		print "login error"
+	else:
+  		print "Logged In"
 
+	return bf
 
-bf = bfpy.BfClient()
-try:
-  bfresp  = bf.login(username=user, password=passw)
-  print "logged in"
-except bfpy.BfError, e:
-  print "login error: %s" % str(e)
-else:
-  print bfresp
+bf = log_in()
 
